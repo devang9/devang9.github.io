efbb8cd50b9723d337637bdea151d4256b0186ee
Devang <devangchheda97@gmail.com>
2020-10-08 20:00:10 +0530

    Updates

---
+++ precache-manifest.6d2d32d781634c4348caca07972b6ef5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59c6b3fe75a101b264262eba233d3c89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f65a2e9ba5a9ad6be4d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "66386ef6da1edeceb823",
+    "url": "/static/js/2.ee973083.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ee973083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f65a2e9ba5a9ad6be4d",
+    "url": "/static/js/main.5ef56cf2.chunk.js"
+  },
+  {
+    "revision": "7914939381f0e7697585",
+    "url": "/static/js/runtime-main.7cdc9915.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
